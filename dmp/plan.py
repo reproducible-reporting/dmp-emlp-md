02d91b1e87676b2c77dea47105581f0bb086245e
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-02-13 09:45:41 +0100

    Update with version 2.0 of the template

---
+++ dmp/plan.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from stepup.core.api import amend, copy, script, static, step
+from stepup.reprep.api import compile_typst
+from yaml import safe_load
+
+# Manually created files used as input.
+static("template.yaml", "fields.yaml", "generate.py")
+
+# Get template information.
+amend(inp="template.yaml")
+with open("template.yaml") as fh:
+    template = safe_load(fh)
+
+# Copy or download the template.
+if "local" in template:
+    static(template["local"])
+    copy(template["local"][-1], "dmp.typ")
+else:
+    version = template["version"]
+    owner = template["github_owner"]
+    repository = template["github_repository"]
+    url = f"https://raw.githubusercontent.com/{owner}/{repository}/{version}/dmp_template.typ"
+    step(f"wget --no-clobber --quiet {url} -O dmp.typ", out="dmp.typ")
+
+# Write the data of the latest Git commit to a yaml file.
+script("generate.py")
+
+# Compile the PDF.
+compile_typst("dmp.typ")
